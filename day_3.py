4e99053adceda0e30d3c0d10c8f7b450c3f016e5
Luke Masterson <info@herosgymtslc.com>
2024-07-06 12:30:20 +0000

    practice

--- day_3.py
+++ day_3.py
@@ -2,7 +2,73 @@ age = 33
 height = 182.88
 complex = 1j
 
-base_triangle = int(input('Enter base of triangle: '))
-height_triangle = int(input('Enter height of triangle: '))
-area_of_triangle = 0.5 * base_triangle * height_triangle
-print(area_of_triangle)
+# base_triangle = int(input('Enter base of triangle: '))
+# height_triangle = int(input('Enter height of triangle: '))
+# area_of_triangle = 0.5 * base_triangle * height_triangle
+# print(area_of_triangle)
+
+# side_a = int(input('Enter side a: '))
+# side_b = int(input('Enter side b: '))
+# side_c = int(input('Enter side c: '))
+# perimeter = side_a + side_b + side_c
+# print(perimeter)
+
+# rectangle_width = int(input('Enter rectangle width: '))
+# rectangle_length = int(input('Enter rectangle length: '))
+# area = rectangle_length * rectangle_width
+# perimeter = 2 * (rectangle_length + rectangle_width)
+# print(area, perimeter)
+
+# circle_radius = int(input('Enter radius: '))
+# pi = 3.14
+# area = pi * circle_radius ** 2
+# circumference = int(2 * pi * circle_radius)
+# print(area, circumference)
+
+# python = 'python'
+# dragon = 'dragon'
+
+# print(len(python) != len(dragon))
+
+# print('on' in 'python' and 'on' in 'dragon')
+
+# string = 'I hope this course is not full of jargon'
+# print('jargon' in string)
+
+# print('on' not in 'python' and 'on' not in 'dragon')
+
+# string = 'python'
+
+# string_length = len(string)
+# print(string_length)
+
+# string_length_float = float(string_length)
+# print(string_length_float)
+
+# string_length_str = str(string_length_float)
+# print(string_length_str, type(string_length_str))
+
+# num = int(input('Enter number: '))
+# check_even = num % 2 == 0
+# print(num , check_even)
+
+# converted_value = int(2.7)
+# num1 = 7
+# num2 = 3
+# floor = num1 // num2
+# check = floor == converted_value
+# print(check)
+
+# print('10' == 10)
+
+# print(int(9.8) == 10)
+
+# hours = int(input('Hours worked that week: '))
+# rate_of_pay = float(input('Hourly rate: '))
+# weekly_pay = float(hours * rate_of_pay)
+# print('Your weekly earnings is', weekly_pay)
+
+# years_lived = int(input('Enter number of years you have lived: '))
+# seconds = years_lived * 365 * 24 * 60 * 60
+# print('You have lived for', seconds, 'seconds')
+
--- day_4.py
+++ day_4.py
@@ -1,7 +1,20 @@
-thirty = 'Thirty'
-days = 'Days'
-of = 'Of'
-python = 'Python'
+# thirty = 'Thirty'
+# days = 'Days'
+# of = 'Of'
+# python = 'Python'
+# space = ' '
 
-full_string = thirty + days + of + python
-print(full_string)
+# full_string = thirty + space + days + space + of + space + python
+# print(full_string)
+
+company = 'Coding For All'
+# print(company)
+# print(len(company))
+# upper_company = company.upper()
+# print(upper_company)
+# lower_company = company.lower()
+# print(lower_company)
+
+# print(company.capitalize(), company.title(), company.swapcase())
+
+print()
